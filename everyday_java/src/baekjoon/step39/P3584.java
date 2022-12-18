bd0a71004f1a68b918749057ba54d20a4ce857ca
seongolee <seongo0521@gmail.com>
2022-12-18 15:43:22 +0900

    백준 P3584 문제 - 가장 가까운 공통 조상

--- everyday_java/src/baekjoon/step39/P3584.java
+++ everyday_java/src/baekjoon/step39/P3584.java
@@ -9,38 +9,44 @@ public class P3584 {
 
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
         StringTokenizer st;
-        int count = Integer.parseInt(br.readLine());
-        parent = new int[count + 1];
-        int answer = 0; // 공통조상 담을 변수
+        int test = Integer.parseInt(br.readLine()); // test 횟수
+        int count;
 
-        for(int i = 0; i < count-1; i++){
+        for(int i=0; i<test; i++){
+            count = Integer.parseInt(br.readLine());
+            parent = new int[count + 1];
+            int temp = 0;
+            for(int j = 0; j < count-1; j++){
+                st = new StringTokenizer(br.readLine());
+                int start = Integer.parseInt(st.nextToken());
+                int end = Integer.parseInt(st.nextToken());
+                parent[end] = start;
+            }
+
             st = new StringTokenizer(br.readLine());
-            int start = Integer.parseInt(st.nextToken());
-            int end = Integer.parseInt(st.nextToken());
-            parent[end] = start;
+            int num1 = Integer.parseInt(st.nextToken());
+            int num2 = Integer.parseInt(st.nextToken());
+
+            Stack<Integer> list1 = rootCheck(num1);
+            Stack<Integer> list2 = rootCheck(num2);
+
+            while(!(list1.isEmpty() || list2.isEmpty())){
+                int temp1 = list1.pop();
+                int temp2 = list2.pop();
+
+                if(temp1 != temp2) break;
+
+                temp = temp1;
+            }
+            sb.append(temp).append("\n");
         }
 
-        st = new StringTokenizer(br.readLine());
-        int num1 = Integer.parseInt(st.nextToken());
-        int num2 = Integer.parseInt(st.nextToken());
-
-        Stack<Integer> list1 = rootCheck(num1);
-        Stack<Integer> list2 = rootCheck(num2);
-
-        while(!(list1.isEmpty() || list2.isEmpty())){
-            int temp1 = list1.pop();
-            int temp2 = list2.pop();
-
-            if(temp1 != temp2) break;
-
-            answer = temp1;
-        }
-
-        System.out.println(answer);
+        System.out.println(sb.toString());
     }
 
-    // 초상위 루트 노드찾기
+    // 최상위 루트 노드찾기
     public static Stack<Integer> rootCheck(int num) {
         Stack<Integer> list = new Stack<>();
         list.push(num);
